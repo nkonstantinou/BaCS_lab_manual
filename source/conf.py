1a8be4ee3eba04e1eee94510218b1370015ffa61
Nikos Konstantinou <nkonstantinou@gmail.com>
2019-09-08 21:13:52 +0300

    updates

--- source/conf.py
+++ source/conf.py
@@ -22,8 +22,7 @@ copyright = '2019, Nikos Konstantinou'
 author = 'Nikos Konstantinou'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
-
+release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 
